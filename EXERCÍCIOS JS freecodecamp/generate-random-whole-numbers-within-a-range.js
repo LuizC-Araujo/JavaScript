8723d6f148bbcb5b6a4b50f0a09e93be5e4ebe27
LuizC-Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2020-08-14 13:22:42 -0300

    Update generate-random-whole-numbers-within-a-range.js
    adicionado comments

--- EXERCÍCIOS JS freecodecamp/generate-random-whole-numbers-within-a-range.js
+++ EXERCÍCIOS JS freecodecamp/generate-random-whole-numbers-within-a-range.js
@@ -1,5 +1,6 @@
+//get random number between max and min
 function randomRange(myMin, myMax) {
-  // Only change code below this line
+  
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-  // Only change code above this line
+  
 }
